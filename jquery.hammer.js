53b27270b19dc101e5dce01f022374bb7bbefbe7
Jeff Andrews <jeff.andrews@carpages.ca>
2017-07-14 20:23:10 -0400

    Carpages-ify repo. Load 'jquery' with gemini-loader

--- jquery.hammer.js
+++ jquery.hammer.js
@@ -1,33 +1,33 @@
-(function(factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'hammerjs'], factory);
-    } else if (typeof exports === 'object') {
-        factory(require('jquery'), require('hammerjs'));
-    } else {
-        factory(jQuery, Hammer);
+( function( factory ) {
+  if ( typeof define === 'function' && define.amd ) {
+    define([ 'gemini', 'hammerjs' ], factory );
+  } else if ( typeof exports === 'object' ) {
+    factory( require( 'gemini-loader' ), require( 'hammerjs' ));
+  } else {
+    factory( G, Hammer );
+  }
+})( function( G, Hammer ) {
+  function hammerify( el, options ) {
+    var $el = G( el );
+    if ( !$el.data( 'hammer' )) {
+      $el.data( 'hammer', new Hammer( $el[0], options ));
     }
-}(function($, Hammer) {
-    function hammerify(el, options) {
-        var $el = $(el);
-        if(!$el.data("hammer")) {
-            $el.data("hammer", new Hammer($el[0], options));
-        }
-    }
+  }
 
-    $.fn.hammer = function(options) {
-        return this.each(function() {
-            hammerify(this, options);
-        });
+  G.fn.hammer = function( options ) {
+    return this.each( function() {
+      hammerify( this, options );
+    });
+  };
+
+  // extend the emit method to also trigger jQuery events
+  Hammer.Manager.prototype.emit = ( function( originalEmit ) {
+    return function( type, data ) {
+      originalEmit.call( this, type, data );
+      G( this.element ).trigger({
+        type: type,
+        gesture: data
+      });
     };
-
-    // extend the emit method to also trigger jQuery events
-    Hammer.Manager.prototype.emit = (function(originalEmit) {
-        return function(type, data) {
-            originalEmit.call(this, type, data);
-            $(this.element).trigger({
-                type: type,
-                gesture: data
-            });
-        };
-    })(Hammer.Manager.prototype.emit);
-}));
+  })( Hammer.Manager.prototype.emit );
+});
